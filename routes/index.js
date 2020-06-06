9d25f9851b8a38a3e4e6b248e73c0b29596a6c38
Um seoyoung <tjdud0123@naver.com>
2020-06-07 01:14:13 +0900

    배너관련 코드완성

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,7 @@ router.use('/user', require('./user'));
 // 챌린지 관련
 router.use('/challenge', require('./challenge'));
 
+// 배너관련
+router.use('/banner', require('./banner'));
 
 module.exports = router;
